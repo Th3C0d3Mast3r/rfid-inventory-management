ed771fbaf1fd3bff60d82291c4d5e03aab09bb6c
Th3C0d3Mast3r <acharyadevesh1105@gmail.com>
2026-01-30 23:11:52 +0530

    dockerized version

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,6 @@ app.get("/", (req, res)=>{
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
